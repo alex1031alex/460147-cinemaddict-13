b906de38e805dce591d39f7ab943902b6036b5dc
Alex <alex1031alex@yandex.ru>
2020-12-13 16:51:18 +0300

    5.2.1 Добавит дата-атрибуты компоненту сортировки, чтобы получить выбранный пользователем тип сортировки.

--- src/view/sort.js
+++ src/view/sort.js
@@ -1,16 +1,25 @@
 import AbstractView from "./abstract.js";
+import {SortType} from "../const.js";
 
 const createSortTemplate = () => (
   `<ul class="sort">
     <li>
-      <a href="#" class="sort__button sort__button--active"
+      <a href="#"
+        class="sort__button sort__button--active" 
+        data-sort-type="${SortType.DEFAULT}"
       >Sort by default</a>
     </li>
     <li>
-      <a href="#" class="sort__button">Sort by date</a>
+      <a href="#"
+        class="sort__button"
+        data-sort-type="${SortType.DATE}"
+      >Sort by date</a>
     </li>
     <li>
-      <a href="#" class="sort__button">Sort by rating</a>
+      <a href="#"
+        class="sort__button"
+        data-sort-type="${SortType.RATING}"
+      >Sort by rating</a>
     </li>
   </ul>`
 );
