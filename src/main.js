fb8779b2efebfdb235c61cadabc4af4890f3206d
Alex <alex1031alex@yandex.ru>
2021-01-02 18:43:00 +0300

    7.1.29 Инициализирует презентер звания пользователя в точке входа. Отрисует с его помощью звание пользователя. Удалит ставший ненужным модуль src/mock/user-rank.js

--- src/main.js
+++ src/main.js
@@ -1,10 +1,9 @@
-import UserProfileView from "./view/user-profile.js";
 import MenuView from "./view/menu.js";
 import CounterView from "./view/counter.js";
 import {generateMovie} from "./mock/movie.js";
-import {generateUserRank} from "./mock/user-rank.js";
 import BoardPresenter from "./presenter/board.js";
 import FilterPresenter from "./presenter/filter.js";
+import UserProfilePresenter from "./presenter/user-profile.js";
 import MoviesModel from "./model/movies.js";
 import FilterModel from "./model/filter.js";
 import {render, RenderPosition} from "./utils/render.js";
@@ -12,10 +11,6 @@ import {render, RenderPosition} from "./utils/render.js";
 const MOVIE_COUNT = 23;
 
 const movies = new Array(MOVIE_COUNT).fill().map(generateMovie);
-const watchedMovies = movies.filter((movie) => movie.userInfo.isWatched);
-
-const userRank = generateUserRank(watchedMovies.length);
-
 const moviesModel = new MoviesModel();
 moviesModel.set(movies);
 
@@ -25,11 +20,12 @@ const siteHeader = document.querySelector(`.header`);
 const siteMain = document.querySelector(`.main`);
 const siteFooter = document.querySelector(`.footer`);
 
-const userProfileComponent = new UserProfileView(userRank);
+const userProfilePresenter = new UserProfilePresenter(siteHeader, moviesModel);
+userProfilePresenter.init();
+
 const menuComponent = new MenuView();
 const counterComponent = new CounterView(movies.length);
 
-render(siteHeader, userProfileComponent, RenderPosition.BEFOREEND);
 render(siteMain, menuComponent, RenderPosition.BEFOREEND);
 
 const siteNavigation = siteMain.querySelector(`.main-navigation`);
